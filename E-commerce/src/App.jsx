e31f48689f8e0a11215834a79e64db770940a354
arabazap47 <arabazpinjar@gmail.com>
2024-05-16 19:21:58 +0530

    started React App

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './Components/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h2>hello</h2>
+      <HomePage />
     </>
   )
 }
